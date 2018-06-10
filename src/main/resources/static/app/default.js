2422d7467dcd1dfacd87749fe8d8fa6aab86bd8e
rosd89 <zkdlffpem@naver.com>
2018-06-10 13:33:55 +0900

    [Create] Box Component 생성

--- src/main/resources/static/app/default.js
+++ src/main/resources/static/app/default.js
@@ -29,3 +29,24 @@ createComponent('v-content-header', {
     template: '#v-content-header',
     props: ['title', 'dep']
 });
+
+createComponent('v-content-body', {
+   template: '#v-content-body'
+});
+
+createComponent('v-box', {
+    template: '#v-box'
+});
+
+createComponent('v-box-header', {
+    template: '#v-box-header',
+    props: ['title']
+});
+
+createComponent('v-box-body', {
+    template: '#v-box-body'
+});
+
+createComponent('v-box-footer', {
+    template: '#v-box-footer'
+});
